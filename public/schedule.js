3c9199d9c2bd3047655a5c28655f32c3af82532f
TimRCAnderson <timothyrcanderson@gmail.com>
2017-06-17 07:08:28 -0400

    added prototype database seed. Need to fix formatting across all static pages. Need to create javascript to pull in API data to create tables for appointments.

--- public/schedule.js
+++ public/schedule.js
@@ -1,27 +1,27 @@
 var dp = new DayPilot.Calendar("dp");
 
-    // view
-    dp.startDate = "2017-06-17";
-    dp.viewType = "Week";
-    
-    // event creating
-    dp.onTimeRangeSelected = function (args) {
-        var name = prompt("Enter Reservation Time", "09:00");
-        if (!name) return;
-        var e = new DayPilot.Event({
-            start: args.start,
-            end: args.end,
-            id: DayPilot.guid(),
-            text: name
-        });
-        dp.events.add(e);
-        dp.clearSelection();
-    };
-    
-    dp.onEventClick = function(args) {
-        alert("clicked: " + args.e.id());
-    };
-    
-    dp.init();
+// view
+dp.startDate = "2017-06-17";
+dp.viewType = "Week";
 
-    dp.events.add(e);
+// event creating
+dp.onTimeRangeSelected = function (args) {
+  var name = prompt("Enter Reservation Time", "09:00");
+  if (!name) return;
+  var e = new DayPilot.Event({
+    start: args.start,
+    end: args.end,
+    id: DayPilot.guid(),
+    text: name
+  });
+  dp.events.add(e);
+  dp.clearSelection();
+};
+
+dp.onEventClick = function(args) {
+  alert("clicked: " + args.e.id());
+};
+
+dp.init();
+
+dp.events.add(e);
